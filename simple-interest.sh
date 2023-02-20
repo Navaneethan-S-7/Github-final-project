3e6cbffb158f01be324257405a9b8ca3b3fa6b5a
Navaneethan-S-77 <125636718+Navaneethan-S-77@users.noreply.github.com>
2023-02-20 14:55:23 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Navaneethan S
    # Additional Authors:
-   # <your GitHub username>
+   # Navaneethan-S-77
 
    # Input:
    # p, principal amount
